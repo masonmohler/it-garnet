db937e026a49912b3e66718ef48803c8297dc140
masonmohler <112035573+masonmohler@users.noreply.github.com>
2024-02-15 19:49:29 +0000

    Adjusted calculator assignment

--- calculator/assets/js/script.js
+++ calculator/assets/js/script.js
@@ -1,22 +1,24 @@
 function doCalculation() {
-  let op1 = parseFloat(document.getElementById("op1").value);
-  let op2 = parseFloat(document.getElementById("op2").value);
+  if ($("#CalculatorForm").valid()) {
+    let op1 = parseFloat(document.getElementById("op1").value);
+    let op2 = parseFloat(document.getElementById("op2").value);
 
-  if (document.getElementById("add").checked) {
-    let result = op1 + op2;
-    document.getElementById("result").innerHTML = result;
-  }
-  if (document.getElementById("subtract").checked) {
-    let result = op1 - op2;
-    document.getElementById("result").innerHTML = result;
-  }
-  if (document.getElementById("multiply").checked) {
-    let result = op1 * op2;
-    document.getElementById("result").innerHTML = result;
-  }
-  if (document.getElementById("divide").checked) {
-    let result = op1 / op2;
-    document.getElementById("result").innerHTML = result;
+    if (document.getElementById("add").checked) {
+      let result = op1 + op2;
+      document.getElementById("result").innerHTML = result;
+    }
+    if (document.getElementById("subtract").checked) {
+      let result = op1 - op2;
+      document.getElementById("result").innerHTML = result;
+    }
+    if (document.getElementById("multiply").checked) {
+      let result = op1 * op2;
+      document.getElementById("result").innerHTML = result;
+    }
+    if (document.getElementById("divide").checked) {
+      let result = op1 / op2;
+      document.getElementById("result").innerHTML = result;
+    }
   }
 }
 
