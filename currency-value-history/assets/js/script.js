2edde112611d96d996d170431011e338b2ac1e8e
masonmohler <112035573+masonmohler@users.noreply.github.com>
2024-02-23 21:33:40 +0000

    Worked on currency value history project

--- currency-value-history/assets/js/script.js
+++ currency-value-history/assets/js/script.js
@@ -1,61 +1,25 @@
-async function GetStock() {
+async function getResults() {
   "use strict";
 
   // Get a reference to the form - Use the ID of the form
-  var form = $("#myform");
+  let form = $("#myform");
 
-  // Validate all of the for elements
+  // Validate all of the form elements
   form.validate();
 
   // If all of the form elements are valid, the get the form values
   if (form.valid()) {
-    var StockSymbol = document.getElementById("StockSymbol").value;
-    var apiKey = "35eaVfKsObXpSg2O4kMLj9udr2DgVW1f";
-    var FromDate = document.getElementById("FromDate").value;
-    var ToDate = document.getElementById("ToDate").value;
+    let baseCurrency = document.getElementById("baseCurrency").value;
+    let toCurrency = document.getElementById("toCurrency").value;
+    let apiKey = "QGs1d8pA7SJAOUFmg0N9A2CZaKxbXLvq";
+    let FromDate = document.getElementById("FromDate").value;
+    let ToDate = document.getElementById("ToDate").value;
 
     /* URL for AJAX Call */
-    var myURL1 =
-      "https://api.polygon.io/v3/reference/tickers/" +
-      StockSymbol +
-      "?apiKey=" +
-      apiKey;
-    /* Make the AJAX call */
-    var msg1Object = await fetch(myURL1);
-    /* Check the status */
-    if (msg1Object.status >= 200 && msg1Object.status <= 299) {
-      var msg1JSONText = await msg1Object.text();
-      // Parse the JSON string into an object
-      var msg1 = JSON.parse(msg1JSONText);
-      /* Your code to process the result goes here - 
-               display the returned message */
-      document.getElementById("company").innerHTML = msg1.results.name;
-      document.getElementById("address").innerHTML =
-        msg1.results.address.address1 +
-        ", " +
-        msg1.results.address.city +
-        ", " +
-        msg1.results.address.state +
-        "   " +
-        msg1.results.address.postal_code;
-      document.getElementById("employees").innerHTML =
-        msg1.results.total_employees;
-      document.getElementById("description").innerHTML =
-        msg1.results.sic_description;
-      document.getElementById("url").innerHTML = msg1.results.homepage_url;
-      document.getElementById("url").href = msg1.results.homepage_url;
-    } else {
-      /* AJAX complete with error - probably invalid stock ticker symbol */
-      /* Your code to process the result goes here - 
-                   display the returned message */
-      alert("Stock Not Found - Status: " + msg1Object.status);
-      return;
-    }
-
-    /* URL for AJAX Call */
-    var myURL2 =
-      "https://api.polygon.io/v2/aggs/ticker/" +
-      StockSymbol +
+    let myURL =
+      "https://api.polygon.io/v2/aggs/ticker/C:" +
+      baseCurrency +
+      toCurrency +
       "/range/1/day/" +
       FromDate +
       "/" +
@@ -63,82 +27,40 @@ async function GetStock() {
       "?unadjusted=false&sort=asc&limit=32&apiKey=" +
       apiKey;
     /* Make the AJAX call */
-    var msg2Object = await fetch(myURL2);
+    let msg1Object = await fetch(myURL);
     /* Check the status */
-    if (msg2Object.status >= 200 && msg2Object.status <= 299) {
-      var msg2JSONText = await msg2Object.text();
+    if (msg1Object.status >= 200 && msg1Object.status <= 299) {
+      let msg1JSONText = await msg1Object.text();
       // Parse the JSON string into an object
-      var msg2 = JSON.parse(msg2JSONText);
+      let msg1 = JSON.parse(msg1JSONText);
       /* Your code to process the result goes here - 
                display the returned message */
-      /* Your code to process the result goes here  
-                    display the returned message */
-      var stockdate = [];
-      var stockvalue = [];
-      var stockvolume = [];
-      var numdays = msg2.results.length;
+      let stockdate = [];
+      let stockvalue = [];
+      let stockvolume = [];
+      let numdays = msg1.results.length;
       if (numdays > 0) {
-        for (var i = 0; i < numdays; i++) {
+        for (let i = 0; i < numdays; i++) {
           /* stock close value */
-          stockvalue[i] = msg2.results[i].c;
+          stockvalue[i] = msg1.results[i].c;
           /* stock volume */
-          stockvolume[i] = msg2.results[i].v;
-          /* date is in Unix milleseconds - create a temporary date variable */
-          var tempdate = new Date(msg2.results[i].t);
+          stockvolume[i] = msg1.results[i].v;
+          /* date is in Unix milleseconds - create a temporary date letiable */
+          let tempdate = new Date(msg1.results[i].t);
           /* extract the date string from the value */
           stockdate[i] = tempdate.toLocaleDateString();
         }
       }
 
-      var stockvaluetable = "";
-      if (numdays > 0) {
-        stockvaluetable =
-          stockvaluetable +
-          "<table><caption>Stock Price</caption><tr><th>Date</th><th>Price</th></tr>";
-        for (var i = 0; i < numdays; i++) {
-          stockvaluetable =
-            stockvaluetable +
-            "<tr><td>" +
-            stockdate[i] +
-            "</td><td>" +
-            stockvalue[i] +
-            "</td></tr>";
-        }
-        stockvaluetable = stockvaluetable + "</table>";
-        document.getElementById("StockValueTable").innerHTML = stockvaluetable;
-      }
-
-      var stockvolumetable = "";
-      if (numdays > 0) {
-        stockvolumetable =
-          stockvolumetable +
-          "<table><caption>Stock Volume</caption><tr><th>Date</th><th>Volume</th></tr>";
-        for (var i = 0; i < numdays; i++) {
-          stockvolumetable =
-            stockvolumetable +
-            "<tr><td>" +
-            stockdate[i] +
-            "</td><td>" +
-            stockvolume[i] +
-            "</td></tr>";
-        }
-        stockvolumetable = stockvolumetable + "</table>";
-        document.getElementById("StockVolumeTable").innerHTML =
-          stockvolumetable;
-      }
-
-      var ctx0 = document.getElementById("chartjs-0");
+      let ctx0 = document.getElementById("chartjs-0");
       var myChart = new Chart(ctx0, {
         type: "line",
         data: {
-          labels: stockdate,
+          labels: dates,
           datasets: [
             {
-              label: "Stock Close",
-              data: stockvalue,
+              data: values,
               fill: false,
-              borderColor: "rgb(75, 192, 192)",
-              lineTension: 0.1,
             },
           ],
         },
@@ -149,35 +71,17 @@ async function GetStock() {
       });
 
       var ctx1 = document.getElementById("chartjs-1");
-      var myChart = new Chart(ctx1, {
-        type: "line",
-        data: {
-          labels: stockdate,
-          datasets: [
-            {
-              label: "Stock Volume",
-              data: stockvolume,
-              fill: false,
-              borderColor: "rgb(75, 192, 192)",
-              lineTension: 0.1,
-            },
-          ],
-        },
-        options: {
-          responsive: false,
-          maintainAspectRatio: true,
-        },
-      });
     } else {
       /* AJAX completed with error - probably invalid stock ticker symbol */
-      alert("Stock Not Found - Status: " + msg2Object.status);
+      alert("Stock Not Found - Status: " + msg1Object.status);
       return;
     }
   }
 }
 
-function ClearForm() {
-  document.getElementById("StockSymbol").value = "";
+function clearForm() {
+  document.getElementById("baseCurrency").value = "";
+  document.getElementById("toCurrency").value = "";
   document.getElementById("FromDate").value = "";
   document.getElementById("ToDate").value = "";
   document.getElementById("company").innerHTML = "";
