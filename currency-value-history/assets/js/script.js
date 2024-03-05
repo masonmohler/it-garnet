eb1d4ce01f258167dc46243c655d00dc227d124c
masonmohler <112035573+masonmohler@users.noreply.github.com>
2024-03-05 01:08:22 +0000

    Finished currency value history assignment

--- currency-value-history/assets/js/script.js
+++ currency-value-history/assets/js/script.js
@@ -35,20 +35,17 @@ async function getResults() {
       let msg1 = JSON.parse(msg1JSONText);
       /* Your code to process the result goes here - 
                display the returned message */
-      let stockdate = [];
-      let stockvalue = [];
-      let stockvolume = [];
+      let currencyDate = [];
+      let currencyValue = [];
       let numdays = msg1.results.length;
       if (numdays > 0) {
         for (let i = 0; i < numdays; i++) {
-          /* stock close value */
-          stockvalue[i] = msg1.results[i].c;
-          /* stock volume */
-          stockvolume[i] = msg1.results[i].v;
+          /* currency close value */
+          currencyValue[i] = msg1.results[i].c;
           /* date is in Unix milleseconds - create a temporary date letiable */
           let tempdate = new Date(msg1.results[i].t);
           /* extract the date string from the value */
-          stockdate[i] = tempdate.toLocaleDateString();
+          currencyDate[i] = tempdate.toLocaleDateString();
         }
       }
 
@@ -56,24 +53,29 @@ async function getResults() {
       var myChart = new Chart(ctx0, {
         type: "line",
         data: {
-          labels: dates,
+          labels: currencyDate,
           datasets: [
             {
-              data: values,
+              label: "1 " + baseCurrency + " to " + toCurrency,
+              data: currencyValue,
               fill: false,
+              borderColor: "rgb(75, 192, 192)",
+              lineTension: 0.1,
             },
           ],
         },
         options: {
-          responsive: false,
+          responsive: true,
           maintainAspectRatio: true,
+          title: {
+            display: true,
+            text: baseCurrency + " to " + toCurrency,
+          },
         },
       });
-
-      var ctx1 = document.getElementById("chartjs-1");
     } else {
       /* AJAX completed with error - probably invalid stock ticker symbol */
-      alert("Stock Not Found - Status: " + msg1Object.status);
+      alert("Currency Not Found - Status: " + msg1Object.status);
       return;
     }
   }
@@ -84,21 +86,9 @@ function clearForm() {
   document.getElementById("toCurrency").value = "";
   document.getElementById("FromDate").value = "";
   document.getElementById("ToDate").value = "";
-  document.getElementById("company").innerHTML = "";
-  document.getElementById("address").innerHTML = "";
-  document.getElementById("employees").innerHTML = "";
-  document.getElementById("ceo").innerHTML = "";
-  document.getElementById("url").innerHTML = "";
-  document.getElementById("url").href = "";
-  document.getElementById("logo").src = "";
-  document.getElementById("StockValueTable").innerHTML = "";
-  document.getElementById("StockVolumeTable").innerHTML = "";
 
   /* Ugly Code to Erase Canvas */
   var canvas0 = document.getElementById("chartjs-0");
   var context0 = canvas0.getContext("2d");
   context0.clearRect(0, 0, canvas0.width, canvas0.height);
-  var canvas1 = document.getElementById("chartjs-1");
-  var context1 = canvas1.getContext("2d");
-  context1.clearRect(0, 0, canvas1.width, canvas1.height);
 }
