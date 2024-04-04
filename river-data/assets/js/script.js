92dee94639c587a473a993f4cc8a4a6244cb4562
masonmohler <112035573+masonmohler@users.noreply.github.com>
2024-04-04 15:52:03 +0000

    Added the ability to select all sites

--- river-data/assets/js/script.js
+++ river-data/assets/js/script.js
@@ -16,12 +16,11 @@ async function getData() {
       "https://waterservices.usgs.gov/nwis/iv/?format=json&sites=" +
       siteChoice +
       "&indent=on&period=P7D&siteStatus=all&parameterCd=00065";
-    /* Make the AJAX call */
     let msg1Object = await fetch(myURL);
-    /* Check the status */
     if (msg1Object.status >= 200 && msg1Object.status <= 299) {
       let msg1JSONText = await msg1Object.text();
       // Parse the JSON string into an object
+      console.log(msg1JSONText);
       let msg1 = JSON.parse(msg1JSONText);
       console.log(msg1);
     } else {
