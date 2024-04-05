2606c966f02263cb1afa77722f2d97d0b9d10cd6
masonmohler <112035573+masonmohler@users.noreply.github.com>
2024-04-05 21:36:04 +0000

    Got chart working for one site

--- river-data/assets/js/script.js
+++ river-data/assets/js/script.js
@@ -16,16 +16,54 @@ async function getData() {
       "https://waterservices.usgs.gov/nwis/iv/?format=json&sites=" +
       siteChoice +
       "&indent=on&period=P7D&siteStatus=all&parameterCd=00065";
-    let msg1Object = await fetch(myURL);
-    if (msg1Object.status >= 200 && msg1Object.status <= 299) {
-      let msg1JSONText = await msg1Object.text();
+    let msgObject = await fetch(myURL);
+    if (msgObject.status >= 200 && msgObject.status <= 299) {
+      let msgJSONText = await msgObject.text();
       // Parse the JSON string into an object
-      console.log(msg1JSONText);
-      let msg1 = JSON.parse(msg1JSONText);
-      console.log(msg1);
+      let msg = JSON.parse(msgJSONText);
+
+      let dates = [];
+      let values = [];
+      /* fLen contains the length of the array (number of values) */
+      let fLen = msg.value.timeSeries[0].values[0].value.length;
+      for (let i = 0; i < fLen; i++) {
+        values[i] = msg.value.timeSeries[0].values[0].value[i].value;
+        dates[i] = new Date(
+          msg.value.timeSeries[0].values[0].value[i].dateTime
+        ).toLocaleString();
+      }
+      let sitename = msg.value.timeSeries[0].sourceInfo.siteName;
+      let sitecode = msg.value.timeSeries[0].sourceInfo.siteCode[0].value;
+      let siteDescription =
+        msg.value.timeSeries[0].variable.variableDescription;
+
+      let ctx0 = document.getElementById("chartjs-0");
+      var myChart = new Chart(ctx0, {
+        type: "line",
+        data: {
+          labels: dates,
+          datasets: [
+            {
+              label: sitename,
+              data: values,
+              fill: false,
+              borderColor: "rgb(75, 192, 192)",
+              lineTension: 0.1,
+            },
+          ],
+        },
+        options: {
+          responsive: false,
+          maintainAspectRatio: true,
+          title: {
+            display: true,
+            text: sitename + " Code: " + sitecode,
+          },
+        },
+      });
     } else {
       /* AJAX completed with error - probably invalid stock ticker symbol */
-      alert("Site Data Not Found - Status: " + msg1Object.status);
+      alert("Site Data Not Found - Status: " + msgObject.status);
       return;
     }
   }
